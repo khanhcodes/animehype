c9044db8b614489e5c0720068423e17973b51648
nhukhanhng <ngkhanh14.kn@gmail.com>
2022-01-01 08:46:20 -0500

    created side bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { useState, useEffect } from 'react';
+import Sidebar from './components/Sidebar';
+import MainContent from './components/MainContent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [animeList, setAnimeList] = useState([]);
+	const [topAnime, setTopAnime] = useState([]);
+	const [search, setSearch] = useState("");
+
+	const getTopAnime = async () => {
+		const temp = await fetch('https://api.jikan.moe/v3/top/anime/1/bypopularity')
+			.then(response => response.json());
+		
+		setTopAnime(temp.top.slice(0,5));
+	}
+
+	useEffect(() => {
+		getTopAnime();
+
+		
+	}, [])
+	console.log(topAnime);
+	return (
+    	<div className="App">
+			<Header/>	
+			<div className="content-wrap">
+				<Sidebar
+					topAnime={topAnime} />
+				<MainContent />
+			</div>
+    	</div>
+	
+  	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <h1><strong>Anime</strong>Hype</h1>
+                <h2>An Anime database for all weebs!</h2>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
